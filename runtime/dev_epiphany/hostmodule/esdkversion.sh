aae8def73030598a3ebb8f42c9d76d029ca69a2e
Ola Jeppsson <ola@adapteva.com>
2016-11-15 14:06:07 +0000

    epiphany: Future proof esdkversion.sh script
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- runtime/dev_epiphany/hostmodule/esdkversion.sh
+++ runtime/dev_epiphany/hostmodule/esdkversion.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Quick and dirty script to get the right compiler and linker flags. :-)
 
-if [ `e-gcc --version | grep -c 2015 | cut -c 1` -eq 1 ]; then
-	if [ $1 = "cflags" ]; then
+if e-gcc --version | grep -q "201[56789]\|master"; then
+	if [ "x$1" = "xcflags" ]; then
 		echo 2015
 	else
 		echo "-le-hal -le-loader"
 	fi
 else
-	if [ $1 = "cflags" ]; then
+	if [ "x$1" = "xcflags" ]; then
 		echo 2013
 	else
 		echo "-le-hal"
